16710744c086604fdab7e64e7f9e8d15900a476c
Roman Balashevich <rbalashevich@gmail.com>
2022-08-18 16:33:16 +0300

    Fixed issue #6
    We want to add new line to file but not overwrite one!
    
    Update exercise1.sh
    
    Fixed issue #5

--- exercise1.sh
+++ exercise1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
 args=("$@")
 echo Number of arguments: $#
 echo 1st argument: ${args[0]}
 echo 2nd argument: ${args[1]}
 echo 3nd argument: ${args[2]}
-echo more arguments: ${args[3]}, ${args[4]}, ${args[5]}
+echo more arguments: ${args[3]}, ${args[4]}, ${args[5]}, ${args[6]}
 
-myArray=("normal" "request" "failure" "error" "warning" "alert" "notice" "note")
+myArray1=(${args[3]} ${args[4]} ${args[5]} ${args[6]})
+myArray2=("normal" "request" "failure" "error" "warning" "alert" "notice" "note")
 echo "Name: $1"
 echo "Count: $2"
 echo "Type: $3"
@@ -22,5 +22,6 @@ if [[ $3 =~ $re && $4 =~ $re && $5 =~ $re && $6 =~ $re ]] ; then
 fi
 
 for (( i = 1; i <= $2; ++i )); do
-  printf "$3: ${myArray[RANDOM%7]}\n" > ./$1.log
+  printf "$i) ${myArray1["$[RANDOM % ${#myArray1[@]}]"]}: ${myArray2[RANDOM%7]}\n" >> ./$1.log
 done
+  printf "\n" >> ./$1.log
