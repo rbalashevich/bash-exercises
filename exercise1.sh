121e5f194f84a04bc029db448472c633a92c77f1
Roman Balashevich <rbalashevich@gmail.com>
2022-08-18 16:33:16 +0300

    Update exercise1.sh
    Fixed issue #6
    
    We want to add new line to file but not overwrite one!
    
    Update exercise1.sh
    
    Fixed issue #5
    
    Multiple fixes
    
    Update exercise1.sh

--- exercise1.sh
+++ exercise1.sh
@@ -1,26 +1,51 @@
 #!/bin/bash
-
 args=("$@")
+re='^[0-9]+$'
+counter=1
+# counteradditional=$(($#-3))
+counterall=$(($#))
+# echo $counteradditional
+
 echo Number of arguments: $#
 echo 1st argument: ${args[0]}
 echo 2nd argument: ${args[1]}
 echo 3nd argument: ${args[2]}
-echo more arguments: ${args[3]}, ${args[4]}, ${args[5]}
+printf "Additional arguments: "
+for (( i = 3; i < $counterall; ++i )); do
+  printf "${args[$i]} "
+done
+printf "\n"
 
-myArray=("normal" "request" "failure" "error" "warning" "alert" "notice" "note")
-echo "Name: $1"
-echo "Count: $2"
-echo "Type: $3"
+ArrayStatus=("normal" "request" "failure" "error" "warning" "alert" "notice" "note")
+
+# echo "Name: $1"
+# echo "Count: $2"
+# echo "Type: $3"
 
 mkdir -p $HOME/mylogs
 cd $HOME/mylogs
-touch $1.log
 
-re='^[0-9]+$'
-if [[ $3 =~ $re && $4 =~ $re && $5 =~ $re && $6 =~ $re ]] ; then
-   echo "error: one of additional parameters is a number and not a string! Exiting." >&2; exit 1
+logfile="$1.log"
+if [ -f "$logfile" ]
+then
+    rm ./${logfile}
+    echo "Old file ${logfile} removed."
 fi
 
-for (( i = 1; i <= $2; ++i )); do
-  printf "$3: ${myArray[RANDOM%7]}\n" > ./$1.log
+touch $1.log
+
+for i in "${args[@]}"; do
+
+echo "🧮 Argument counter in array is $counter now"
+echo "🔬 Dumping argument, it is \"$i\" now. Checking is it a numeric or not"
+if [[ $counter -gt 2 && $i =~ $re ]] ; then
+   echo "🔢 Error: one of additional parameters is numeric and not a string! Exiting." >&2
+   exit 1
+else 
+ for (( i = 3; i < $counterall; ++i )); do
+   printf "${args[$i]}: ${ArrayStatus[RANDOM%7]}\n" >> ./$1.log
+ done
+fi
+counter=$((counter+1))
+
 done
