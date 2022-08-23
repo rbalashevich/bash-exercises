ef9907ffa01b52a7485d66ec5270778e57e177bb
Roman Balashevich <rbalashevich@gmail.com>
2022-08-23 18:43:30 +0300

    Multiple fixes

--- exercise1.sh
+++ exercise1.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 args=("$@")
 echo Number of arguments: $#
 echo 1st argument: ${args[0]}
@@ -6,22 +6,38 @@ echo 2nd argument: ${args[1]}
 echo 3nd argument: ${args[2]}
 echo more arguments: ${args[3]}, ${args[4]}, ${args[5]}, ${args[6]}
 
-myArray1=(${args[3]} ${args[4]} ${args[5]} ${args[6]})
-myArray2=("normal" "request" "failure" "error" "warning" "alert" "notice" "note")
+ArrayStatus=("normal" "request" "failure" "error" "warning" "alert" "notice" "note")
 echo "Name: $1"
 echo "Count: $2"
 echo "Type: $3"
 
 mkdir -p $HOME/mylogs
 cd $HOME/mylogs
+
+logfile="$1.log"
+if [ -f "$logfile" ]
+then
+    rm ./${logfile}
+    echo "Old file ${logfile} removed."
+fi
+
 touch $1.log
 
 re='^[0-9]+$'
-if [[ $3 =~ $re && $4 =~ $re && $5 =~ $re && $6 =~ $re ]] ; then
-   echo "error: one of additional parameters is a number and not a string! Exiting." >&2; exit 1
-fi
+counter=1
+# counteradditional=$(($#-3))
+counterall=$(($#))
+# echo $counteradditional
 
-for (( i = 1; i <= $2; ++i )); do
-  printf "$i) ${myArray1["$[RANDOM % ${#myArray1[@]}]"]}: ${myArray2[RANDOM%7]}\n" >> ./$1.log
+for i in "${args[@]}"; do
+
+echo "🧮 Argument counter in array is $counter now"
+echo "🔬 Dumping argument, it is \"$i\" now. Checking is it a numeric or not"
+if [[ $counter -gt 2 && $i =~ $re ]] ; then
+   echo "🔢 Error: one of additional parameters is numeric and not a string! Exiting." >&2; exit 1
+fi
+ for (( i = 3; i < $counterall; ++i )); do
+   printf "${args[$i]}: ${ArrayStatus[RANDOM%7]}\n" >> ./$1.log
+ done
+counter=$((counter+1))
 done
-  printf "\n" >> ./$1.log
