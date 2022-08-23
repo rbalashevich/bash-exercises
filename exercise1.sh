25f24cd9cf9ba9dd2a5c6603df6346dded878a65
Roman Balashevich <rbalashevich@gmail.com>
2022-08-24 00:26:19 +0300

    Redesigned

--- exercise1.sh
+++ exercise1.sh
@@ -1,51 +1,43 @@
-#!/bin/bash
-args=("$@")
-re='^[0-9]+$'
-counter=1
-# counteradditional=$(($#-3))
-counterall=$(($#))
-# echo $counteradditional
+#!/usr/bin/env bash
 
-echo Number of arguments: $#
-echo 1st argument: ${args[0]}
-echo 2nd argument: ${args[1]}
-echo 3nd argument: ${args[2]}
-printf "Additional arguments: "
-for (( i = 3; i < $counterall; ++i )); do
-  printf "${args[$i]} "
-done
-printf "\n"
+script_args=("$@")
+
+name="${script_args[0]}"
+count="${script_args[1]}"
+args=("${script_args[@]:3}") # https://stackoverflow.com/a/1336245
+
+echo "Number of arguments: $#"
+echo "Name: ${name}"
+echo "Count: ${count}"
+echo "Additional arguments: ${args[@]}"
 
 ArrayStatus=("normal" "request" "failure" "error" "warning" "alert" "notice" "note")
 
-# echo "Name: $1"
-# echo "Count: $2"
-# echo "Type: $3"
+log_folder="${HOME}/mylogs"
+log_file="${name}.log"
+log_path="${log_folder}/${log_file}"
 
-mkdir -p $HOME/mylogs
-cd $HOME/mylogs
-
-logfile="$1.log"
-if [ -f "$logfile" ]
+mkdir -p "${log_folder}"
+if [ -f "${log_path}" ]
 then
-    rm ./${logfile}
-    echo "Old file ${logfile} removed."
+  rm "${log_path}"
+  echo "Old file ${log_path} removed."
 fi
-
-touch $1.log
+touch "${log_path}"
 
 for i in "${args[@]}"; do
+  echo "🔬 Dumping argument, it is \"$i\" now. Checking is it a numeric or not"
+  if [[ "$i" ]] && [[ -z "${i//[0-9]}" ]]; then # https://stackoverflow.com/a/61835747
+    echo "🔢 Error: one of additional parameters is numeric and not a string! Exiting." >&2
+    exit 1
+  fi
+done
 
-echo "🧮 Argument counter in array is $counter now"
-echo "🔬 Dumping argument, it is \"$i\" now. Checking is it a numeric or not"
-if [[ $counter -gt 2 && $i =~ $re ]] ; then
-   echo "🔢 Error: one of additional parameters is numeric and not a string! Exiting." >&2
-   exit 1
-else 
- for (( i = 3; i < $counterall; ++i )); do
-   printf "${args[$i]}: ${ArrayStatus[RANDOM%7]}\n" >> ./$1.log
- done
-fi
-counter=$((counter+1))
+args_multiplier=${args[@]}
+for ((i=1; i<${count}; i++)); do
+  args+=(${args_multiplier[@]})
+done
 
+for i in "${args[@]}"; do
+  echo "$i: ${ArrayStatus[RANDOM%7]}" >> "${log_path}"
 done
